83ea29b78405a24155d3c0e94c4ac83ebdd6c489
Samuel Thurman <sam.thurman@hotmail.com>
2020-04-19 17:06:33 -0700

    git commit - Branch 1
    Terminal Push Test
    
    Commit also has script developments

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    let coinManager = CoinManager()
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self
+    
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return coinManager.currencyArray[row]
+        }
+    
     }
-
+    for i in 1...10 {
+    while true {
+    print("Hello there!")
+    if i == 5
+    break
+    }
+    }
 
 }
 
+// Test
